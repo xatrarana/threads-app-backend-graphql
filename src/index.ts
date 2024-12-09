a389806fa37b5665e26c79672e7a2469259f3a7c
xatrarana <chhatraclas@gmail.com>
2024-12-09 18:59:20 +0545

    Database and ORM

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express, { query } from "express";
 import { ApolloServer } from "@apollo/server";
 import { expressMiddleware } from '@apollo/server/express4';
 import cors from "cors";
+import { prismaClient } from "./lib/db";
 
 async function init() {
     const app = express();
@@ -18,6 +19,16 @@ async function init() {
                 hello: String
                 say(name:String): String
             }
+
+
+            type Mutation {
+                createUser(
+                    firstName: String!,
+                    lastName: String!,
+                    email: String!,
+                    password: String!
+                ): Boolean
+            }
             
         `, // Schema
         resolvers: {
@@ -25,8 +36,31 @@ async function init() {
                 hello: () => "Hey there I'm graphQl server",
                 say: (_, {name} : {name: string}) =>  ` Hey ${name}. How are you?`
             },
+
+            Mutation: {
+                createUser: async(_,{
+                    firstName,lastName,email,password
+                }:{
+                    firstName: string, lastName: string, email: string, password: string
+                }) => {
+                    await prismaClient.user.create({
+                        data: {
+                            firstName,
+                            lastName,
+                            email,
+                            password,
+                            salt:"random_salt"
+                        }
+                    })
+
+                    return true;
+                }
+            }
            
         }  // folding logic
+
+
+
     });
 
     // start gqlServer
